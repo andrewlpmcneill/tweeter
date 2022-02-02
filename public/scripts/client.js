3d12c7806125d6f3efc6423ba0da5d7a248ad2b2
Andrew McNeill <andrew@andrewmc.ca>
2022-02-02 18:15:37 -0400

    Adds submit tweet handling and AJAX GET requests for data + rendering

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,6 +7,23 @@
 // eslint-disable-next-line no-undef
 $(document).ready(function() {
 
+  $('#submitTweet').on('submit', (event => {
+    console.log('Handler for submit() called');
+    event.preventDefault();
+    const data = $('#tweet-text');
+    if (!data.val() || data.val().length > 140) {
+      alert('Tweet content is empty, or too long');
+      return;
+    }
+    const serializedData = data.serialize();
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: serializedData
+    });
+
+  }));
+  
   const renderTweets = tweetArr => {
     tweetArr.forEach(tweet => {
       $('.container').append(createTweetElement(tweet));
@@ -19,7 +36,7 @@ $(document).ready(function() {
     const avatar = tweetObj['user']['avatars'];
     const handle = tweetObj['user']['handle'];
     const content = tweetObj['content']['text'];
-    const time = tweetObj['content']['created_at'];
+    const time = tweetObj['created_at'];
 
     const output = `<article class="tweet-container">
         <header>
@@ -50,32 +67,15 @@ $(document).ready(function() {
   
   };
 
+  const loadTweets = () => {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function(data) {
+        renderTweets(data);
+      });
 
-  renderTweets(data);
+  };
+
+  loadTweets();
   
 });
