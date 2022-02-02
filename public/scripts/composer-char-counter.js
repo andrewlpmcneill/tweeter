c46df6df384037e065d48be39b7f6ba855936bf2
Andrew McNeill <andrew@andrewmc.ca>
2022-02-02 14:42:11 -0400

    Refactors script

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
   
-  $('#tweet-text').on('input', null, function() {
+  $('#tweet-text').on('input', function() {
     const numCharactersTyped = $(this).val().length;
     const count = 140 - numCharactersTyped;
     const counter = $(this).parent().children('.submission-info').children('.counter');
-    $(counter).html(String(count));
+    counter.val(count);
     if (count < 0) {
-      $(counter).addClass("charactersExceeded");
+      counter.addClass("charactersExceeded");
       return;
     }
-    $(counter).removeClass("charactersExceeded");
+    counter.removeClass("charactersExceeded");
   });
 
 });
