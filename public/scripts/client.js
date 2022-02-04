8efd8c6579a31ed2df8528deea517f7912309fc6
Andrew McNeill <andrew@andrewmc.ca>
2022-02-03 22:03:45 -0400

    Adds comments, and restructures JS files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,110 +1,44 @@
+// This file contains all the logic around manipulating DOM elements,
+// hiding and unhiding elements, and general set up
+
 $(document).ready(function() {
 
+  // Immediately set error messages, alternative icons, and "to top" functionality to :hidden
   $('.error').hide();
   $('.fa-angle-double-up').hide();
   $('.to-top').hide();
 
-  $('#submitTweet').on('submit', (event => {
-    event.preventDefault();
-    const data = $('#tweet-text');
-    if (!data.val()) {
-      $('#taEmpty').show();
-      return;
-    } else if (data.val().length > 140) {
-      $('#taOverMax').show();
+  // Logic for text area's character counter
+  $('#tweet-text').on('input', function() {
+    $('.error').hide();
+    let scrollHeight = $('#tweet-text').get(0).scrollHeight;
+    $('#tweet-text').css('height', scrollHeight + 'px');
+    const numCharactersTyped = $(this).val().length;
+    const count = 140 - numCharactersTyped;
+    const counter = $(this).parent().children('.submission-info').children('.counter');
+    counter.val(count);
+    if (count < 0) {
+      counter.addClass("charactersExceeded");
       return;
     }
-    const serializedData = data.serialize();
-    $.post("/tweets", serializedData)
-      .then(function(data) {
-        $("#tweet-text").val('');
-        loadTweets('test');
-      });
-  }));
-
-  $('.new-tweet-toggle').on('click', function() {
-    $('.new-tweet').slideToggle();
-    $('.fa-angle-double-down').toggle();
-    $('.fa-angle-double-up').toggle();
+    counter.removeClass("charactersExceeded");
   });
 
+  // When user scrolls on the page, show an element that, when clicked, will take them back to the top
   $(window).scroll(function() {
+    // Un-hide 'to top' icon
     $('.to-top').fadeIn();
+    // If the user is at the top of the page, hide the icon again
     if ($(window).scrollTop() === 0) {
       $('.to-top').fadeOut(350);
     }
   });
-
+  
+  // When the 'to top' icon is clicked, scroll to the top of the page
   $('.footer-icon').on('click', function() {
     $('html').animate({
       scrollTop: 0
     }, "slow");
   });
-  
-  const renderTweets = tweetArr => {
-    tweetArr.forEach(tweet => {
-      $('.data-container').prepend(createTweetElement(tweet));
-    });
-  };
-
-  const createTweetElement = tweetObj => {
-    const name = tweetObj['user']['name'];
-    const avatar = tweetObj['user']['avatars'];
-    const handle = tweetObj['user']['handle'];
-    const content = tweetObj['content']['text'];
-    const safeContent = escape(content);
-    const time = tweetObj['created_at'];
-
-    const output = `<article class="tweet-container">
-        <header>
-          <div>
-            <img src="${avatar}">
-            <h4>${name}</h4>
-          </div>
-          <div>
-            <p class="handle">${handle}</p>
-          </div>
-        </header>
-        <div class="tweet-body">
-          <p>${safeContent}</p>
-        </div>
-        <footer>
-          <div class="date">
-            <span>${timeago.format(time)}</span>
-          </div>
-          <div class="icons">
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fas fa-heart"></i>
-          </div>
-        </footer>
-      </article>`;
-  
-    return output;
-  };
-
-  const loadTweets = (ifNew) => {
-    if (ifNew) {
-      $.ajax('/tweets', { method: 'GET' })
-        .then(function(data) {
-          renderTweets([data[data.length - 1]]);
-        });
-      return;
-    }
-    // $('.data-container').empty();
-    $.ajax('/tweets', { method: 'GET' })
-      .then(function(data) {
-        renderTweets(data);
-      });
-
-  };
-
-  const escape = function(str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
-  loadTweets();
   
 });
